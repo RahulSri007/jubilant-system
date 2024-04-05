63760af9ec9691d2040066e43814714214e7b24e
RahulSri007 <166106094+RahulSri007@users.noreply.github.com>
2024-04-05 17:55:03 +0530

    Update streamlit_app.py
    Use Our fruit_chosen Variable in the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
-# Write directly to the app
 st.title(":heart: Customize your smoothie :cup_with_straw:")
 st.write(
     """Choose the fruit you want in your custom Smoothie!
@@ -26,6 +26,9 @@ if ingredients_list:
 
   for fruit_chosen in ingredients_list:
      ingredients_string += fruit_chosen + ' '
+     st.subheader(fruit_chosen + ' Nutrition Information') 
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+     fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True) 
 
   my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
@@ -37,8 +40,5 @@ if ingredients_list:
      session.sql(my_insert_stmt).collect()
       
      st.success('Your Smoothie is ordered', icon="✅")  
-    
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# st.text(fruityvice_response.json())
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+
